8204e98d8becdbf33c0ab3f8069644c3298e3b8b
Jn627 <127040323+Jn627@users.noreply.github.com>
2024-10-22 13:41:20 +0100

    Add files via upload

---
+++ databases part2.py
@@ -0,0 +1,47 @@
+x = float(input('enter value for x'))
+y = float(input('enter value for y'))
+z = float(input('enter value for z'))
+xy = x+y
+yz = y+z
+xz = x+y
+xyz = x*y*z
+ans = xyz/(xy+yz+xz)
+print(ans)
+
+num1 = int(input('enter a number'))
+num2 = int(input('enter a number'))
+div = num1/num2
+print(div)
+mul = num1*num2
+print(mul)
+add = num1+num2
+print(add)
+sub = num1-num2
+print(sub)
+floor = num1//num2
+print(floor)
+power = num1**num2
+print(power)
+
+num = float(input('enter a number'))
+num1 = float(input('enter a number'))
+num2 = float(input('enter a number'))
+ans = num+num1+num2
+finalans = ans/3
+print(finalans)
+
+r1 = 7
+pie = 3.14
+num = 4/3
+ans = num*pie*r1**3
+print(ans)
+r2 = 12
+pie = 3.14
+num = 4/3
+ans = num*pie*r1**3
+print(ans)
+r3 = 16
+pie = 3.14
+num = 4/3
+ans = num*pie*r1**3
+print(ans)
