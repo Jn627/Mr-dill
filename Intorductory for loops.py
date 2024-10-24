c06be3232ba766645dcde708d1803ecb137146f4
Jn627 <127040323+Jn627@users.noreply.github.com>
2024-10-24 12:01:56 +0100

    Add files via upload

---
+++ Intorductory for loops.py
@@ -0,0 +1,21 @@
+side = float(input('enter side 1'))
+side2 = float(input('enter side 2'))
+side3 = float(input('enter side 3'))
+if side==side2==side3:
+  print('equal')
+if side!=side2!=side3:
+  print('scalene')
+if side==side2!=side3 or side2==side3!=side or side==side3!=side2:
+    print('isolceles')
+num = int(input('enter a number'))
+dad = num%10
+if dad==4 or dad==8:
+ print(dad)
+ 
+N = int(input('enter a number greater than 20 big boy'))
+for i in range(11,N+1,1):
+    print(i)
+if
+
+
+
