f45eaaf16444392ad2699beee7c4122f27b3eb2d
Jn627 <127040323+Jn627@users.noreply.github.com>
2024-11-05 13:38:21 +0000

    Add files via upload

---
+++ for loops 2.py
@@ -0,0 +1,41 @@
+num = int(input('enter a interger'))
+running_total = 0
+num_inputs = 0
+if num >= 0:
+   while num >0:
+    running_total = num + running_total
+    num_inputs =num_inputs + 1
+    num = int(input('enter an interger'))
+ans = running_total/num_inputs
+print(ans)
+
+num = int(input('enter a interger'))
+running_total = 0
+num_inputs = 0
+if num >= -1:
+   while num >-1:
+    running_total = num + running_total
+    num_inputs =num_inputs + 1
+    num = int(input('enter an interger'))
+ans = running_total/num_inputs
+print(ans)
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+
